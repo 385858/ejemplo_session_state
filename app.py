b0439d844611f039b123d263fb6d72d73c89c4f2
385858 <a385858@uach.mx>
2025-10-29 08:05:07 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,5 @@ if "count" not in st.session_state:
   st.session_state["count"] = 0
 #count = 0
 
-increment = st.button("Increment")
-if increment:
-  count += 1
-
-st.write("Count = ", count)
 
 st.write(st.session_state)
